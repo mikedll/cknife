0dbf9ecc9206d3db5e9751e9937186a298feb98e
Mike De La Loza <mikedll@mikedll.com>
2014-04-01 09:38:00 -0700

    Can use yml file for config.

--- aws.rb
+++ aws.rb
@@ -13,16 +13,42 @@ require 'digest/md5'
 
 class Aws < Thor
 
-  KEY = ENV["KEY"] || ENV['AMAZON_ACCESS_KEY_ID']
-  SECRET = ENV["SECRET"] || ENV['AMAZON_SECRET_ACCESS_KEY']
 
   no_tasks do
+
+    def config
+      return @config if @config
+
+      @config = {
+        :key => ENV["KEY"] || ENV['AMAZON_ACCESS_KEY_ID'], 
+        :secret => ENV["SECRET"] || ENV['AMAZON_SECRET_ACCESS_KEY']
+      }
+      
+      config_file = nil
+      Pathname.new(Dir.getwd).tap do |here|
+        config_file = [["cknife.yml"], ["tmp", "cknife.yml"]].map { |args|
+          here.join(*args)
+        }.select { |path|
+          File.exists?(path) 
+        }.first
+      end
+
+      if config_file
+        begin
+          @config.merge!(YAML.load(config_file.read).symbolize_keys!)
+        rescue
+          say ("Found, but could not parse config: #{config_file}")
+        end        
+      end
+
+      @config
+    end
 
     def fog_opts
       opts = {
         :provider => 'AWS',
-        :aws_access_key_id => KEY,
-        :aws_secret_access_key => SECRET
+        :aws_access_key_id => config[:key],
+        :aws_secret_access_key => config[:secret]
       }      
       opts.merge!({ :region => options[:region] }) if !options[:region].blank?
       opts
